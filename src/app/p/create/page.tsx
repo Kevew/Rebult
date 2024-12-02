5ae5b649af28b6d08a05bb0f42e4bb598d984a3a
FrederickPu <pufrederick51@gmail.com>
2024-12-01 20:24:59 -0500

    try again

--- src/app/p/create/page.tsx
+++ src/app/p/create/page.tsx
@@ -4,7 +4,6 @@ import { Button } from '@/components/ui/Button';
 import { Input } from '@/components/ui/Input';
 import { toast } from '@/hooks/use-toast';
 import { useCustomToasts } from '@/hooks/use-custom-toasts';
-import { CreateSubredditPayload } from '@/lib/validators/subreddit';
 import { useMutation } from '@tanstack/react-query';
 import axios, { AxiosError } from 'axios';
 import { useRouter } from 'next/navigation';
