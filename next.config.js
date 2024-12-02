cbbe4301a015cd5afac838e2f5d6577854963e00
FrederickPu <pufrederick51@gmail.com>
2024-12-01 20:30:43 -0500

    try again v2

--- next.config.js
+++ next.config.js
@@ -13,6 +13,16 @@ const nextConfig = {
     config.externals.push({ canvas: 'commonjs canvas' }) // https://github.com/Automattic/node-canvas/issues/867
     return config
   },
+  env: {
+    REDIS_URL: process.env.REDIS_URL,
+    REDIS_SECRET: process.env.REDIS_SECRET,
+    UPLOADTHING_APP_ID: process.env.UPLOADTHING_APP_ID,
+    UPLOADTHING_SECRET: process.env.UPLOADTHING_SECRET,
+    GOOGLE_CLIENT_SECRET: process.env.GOOGLE_CLIENT_SECRET,
+    GOOGLE_CLIENT_ID: process.env.GOOGLE_CLIENT_ID,
+    NEXTAUTH_SECRET: process.env.NEXTAUTH_SECRET,
+    DATABASE_URL: process.env.DATABASE_URL
+  },
 }
 
 module.exports = nextConfig
